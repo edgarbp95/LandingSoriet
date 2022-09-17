bc241f27de20379d870060d771af9ec0b0711c8d
edgarbp95 <edgarbp95@hotmail.com>
2022-09-17 17:10:52 -0300

    act

--- src/components/SocialMenu.jsx
+++ src/components/SocialMenu.jsx
@@ -7,7 +7,7 @@ const SocialMenu = ({style,styleIcon}) => {
   return (
     <div className={`h-16 ${style} relative bottom-6 rounded-xl flex items-center text-white gap-4`}>
           <a href="https://www.instagram.com/spcreativo/" target="_blank" className={`${styleIcon}`}><RiInstagramFill size="25px" /></a>
-          {/* <a href="" className={`${styleIcon}`}><FaFacebookF size="25px"/></a> */}
+          <a href="https://www.facebook.com/profile.php?id=100085845730361" target="_blank"className={`${styleIcon}`}><FaFacebookF size="25px"/></a>
           <a href="https://wa.me/+584141062111?text=Hola!%20Necesito%20un%20presupuesto%20de%20diseño.%20" target="_blank" className={`${styleIcon}`}><IoLogoWhatsapp size="25px" /></a>
     </div>
   )
